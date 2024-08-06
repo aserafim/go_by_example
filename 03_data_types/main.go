1ab8ee527f1238b0ac336d3831f531b2873bbcea
aserafim <correia.alefe@gmail.com>
2024-08-06 14:02:06 -0300

    Data Types

---
+++ 03_data_types/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Data Types")
+
+	var a bool = true
+	var b int = 4
+	var c float32 = 3.24
+	var d float64 = 5.984
+	var e string = "Hi!"
+	var f complex64 = (3 + 2i)
+
+	fmt.Println("Boolean: ", a)
+	fmt.Println("Int: ", b)
+	fmt.Println("Float32: ", c)
+	fmt.Println("Float 64: ", d)
+	fmt.Println("String: ", e)
+	fmt.Println("Complex :", f)
+}
