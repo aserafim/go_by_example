2ed0574832263922bc6d7daebda9a3427ed0c38a
aserafim <correia.alefe@gmail.com>
2024-08-07 13:25:56 -0300

    Criada função para calcular IMC

--- extras/main.go
+++ extras/main.go
@@ -6,6 +6,8 @@ import (
 )
 
 var DivideByZero = errors.New("division by zero")
+var PesoNaoAceito = errors.New("peso deve ser maior que zero")
+var AlturaNaoAceita = errors.New("altura deve ser maior que zero")
 
 func Divide(a, b float32) (ret float32, err error) {
 
@@ -16,8 +18,29 @@ func Divide(a, b float32) (ret float32, err error) {
 	return a / b, nil
 }
 
+func calculaIMC(peso float64, altura float64) (res float64, err error) {
+
+	if peso <= 0 {
+		return 0, PesoNaoAceito
+	}
+
+	if altura <= 0 {
+		return 0, AlturaNaoAceita
+	}
+
+	var imc = peso / (altura * altura)
+
+	return imc, nil
+
+}
+
 func main() {
 
 	fmt.Println(Divide(2,3))
+
+	altura := 1.83
+	peso := 60.0
+	var resultado,_ = calculaIMC(peso, altura)
+	fmt.Printf("O IMC é: %.2f.",resultado)
 
 }
